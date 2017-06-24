e7cd58592d5edeca565211904e6e3c8b2cdbeeee
Alexander Mogilka <alxmfr0st@gmail.com>
2017-06-24 23:35:14 +0300

    Initial commit

---
+++ Consumer/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+type ConsumerSettings = {
+    KafkaUrl : string
+    Topic : string
+}
+
+let rec loop action =
+    async {
+        action()
+        do! Async.Sleep 5000
+
+        return! loop action 
+    }
+
+[<EntryPoint>]
+let main argv = 
+    let settings = {
+        KafkaUrl = "localhost:9092"
+        Topic = "download-completed-v1"
+    }
+
+    fun () -> printfn "[Consumer]: received a message from Kafka at %A" DateTime.Now
+    |> loop
+    |> Async.RunSynchronously
+
+
+    0
---
+++ Producer/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+type ProducerSettings = {
+    KafkaUrl : string
+    Topic : string
+    Timeout : int
+}
+
+let rec loop timeout action =
+    async {
+        action()
+
+        do! Async.Sleep timeout
+
+        return! loop timeout action 
+    }
+
+[<EntryPoint>]
+let main argv = 
+    let settings = {
+        KafkaUrl = "localhost:9092"
+        Topic = "download-completed-v1"
+        Timeout = 5000
+    }
+
+    fun () -> printfn "[Producer]: Sending a message to Kafka at %A" DateTime.Now
+    |> loop settings.Timeout
+    |> Async.RunSynchronously
+    
+    0
